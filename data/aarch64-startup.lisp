44771dd87534205fd5b9c1fad6b05d9d5b15d87e
ivg <ivg@ieee.org>
2021-11-19 14:54:01 -0500

    removes unnecessary initialization procedure for aarch-crc32
    It was never called and was originally wrought for debugging this file
    under the normal executor, but the it was really breaking the symbolic
    executor, except that before BinaryAnalysisPlatform/bap#1370 it was
    never called (yet another reason to merge it, as it looks like from
    the way how we use methods we were assuming the behavior that bap#1370
    just introduces).

--- data/aarch64-startup.lisp
+++
@@ -1,6 +0,0 @@
-(defconstant +symexec-crc32-channels+ 0x10fd0)
-
-(defmethod init ()
-  (let ((stdin (malloc (sizeof ptr_t))))
-    (write-word ptr_t +symexec-crc32-channels+ stdin)
-    (write-word ptr_t stdin 0)))
